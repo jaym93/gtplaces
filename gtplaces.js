9451fa08b73c909ef4f3557f13161328b4480c90
Andrew Douglas Nelson <anelson35@mail.gatech.edu>
2013-08-02 18:48:20 +0000

    Fixed tagging

--- gtplaces.js
+++ gtplaces.js
@@ -1,5 +1,5 @@
 //TODO: Hide tag button if not logged in
-//TODO: permalink on placeInfoMain
+//TODO: Make tags show on building page
 
 
 var test;
@@ -11,16 +11,42 @@ var tags;
 
 
 $(document).ready(function() {
-	body_onload();
+    //Actions
+	$("#add_tag_link").on('click', addTag);
+	$("#new_tag").on('keypress', function(e) {
+	    if (e.keyCode == 13) { //13 == Enter
+	        saveTag();
+        }
+	});
+	$("#tag_button").on('click', saveTag);
+	$("#cancel_new_tag").on('click', function() {
+        $("#tag_input").hide();
+    });
 	
+	//Load building data from localStorage or download from server
+	if(placesJSONText = localStorage.getItem('OfflineGTplaces')) {
+	    console.log("Loading data from localStorage");
+		loadPlaces(placesJSONText);
+	} else {
+	    console.log("Fetching data from servers");
+		init();
+	}
+	
+	//Permalink handling
+	var bid = $.url().fparam("bid");
 	if ("placeInfoMain" === $.mobile.activePage.attr('id')) {
-	    //Put name of building with supplied building id in search field
-        var bid = $.url().fparam("bid");
+	    //Load data for building with supplied building id
         if (bid !== undefined && bid.length > 0) {
+            console.log("Loading building via permalink");
             searchResultClick(bid);
         }
-	} else if ("placeListMain" === $.mobile.activePage.attr('id')) {   
-	    $('#searchResultsList').listview('refresh');
+	} else if ("placeListMain" === $.mobile.activePage.attr('id')) {
+	    //Put name of building with supplied building id in search field
+        if (bid !== undefined && bid.length > 0) {
+            console.log("Loading building via permalink");
+            $("input[data-type='search']").val(buildings[bid].name).trigger('change');
+        }
+        $('#searchResultsList').listview('refresh');
     }
 });
 
@@ -36,38 +62,16 @@ function addTag() {
 }
 
 
-function body_onload() {	
-	$("#add_tag_link").on('click', addTag);
-	$("#tag_button").on('click', saveTag);
-	$("#cancel_new_tag").on('click', function() {
-        $("#tag_input").hide(); 
-    });
-	
-	try {
-		if(placesJSONText = localStorage.getItem('OfflineGTplaces')) {
-		    console.log("Loading data from localStorage");
-			loadPlaces(placesJSONText);
-		} else {
-		    console.log("Fetching data from servers");
-			init();
-		}
-		return;
-	} catch (err) {
-		alert("Error occurred! - " + err);
-		console.log("Error Occurred:");
-		console.log(err);
-	}
-}
-
-
-function confirmFlagTag(e) {
-    var selectedTag = $.trim($(e).text());
-	var selectedTagId = $(e).attr("id");
+function confirmFlagTag(tagSpan) {
+    var selectedTag = $.trim($(tagSpan).text());
+	var selectedTagId = $(tagSpan).attr("id");
 	$("#txtFlag").empty();
-	$("#txtFlag").append(selectedTag); 
+	$("#txtFlag").append(selectedTag);
 	
 	$("#btnFlag").click(function() {
-        $.post("api/tags/" + selectedTag +"/flag", { "bid": currentPlaceID}, function(data) {
+        $.post("api/tags/" + selectedTag + "/flag", {"bid": currentPlaceID}, function(data) {
+            console.log(data);
+            test = data;
             $("#"+selectedTagId).find('a').css("color", "red");
             $("#"+selectedTagId).css("font-size", "12px");
         });
@@ -75,30 +79,20 @@ function confirmFlagTag(e) {
 }
 
 
-/*
-Modified by Janani Narayanan
-****************************
-Function to query buildings to display tags 
-for the selected building.
-*/
-function getPlaceTags(placeID, callback) {
-    jQuery.map(tags, function (obj) {
-        if(obj.b_id == placeID) {
-            callback(obj);
-        }
-    });
-}
-
-
 function init() {
    // Put the object into storage - added by Janani Narayanan
    if(supports_html5_storage()) {
-	   	localStorage.clear();
-		$.getJSON("api/buildings/", function(data) {
-		    console.log("Downloaded buildings");
-			localStorage.setItem('OfflineGTplaces', JSON.stringify(data));
-		    loadPlaces();
-		});
+        $.ajax({
+            url: "api/buildings/",
+            dataType: 'json',
+            async: false,
+            success: function(data) {
+                console.log("Downloaded buildings");
+                localStorage.clear();
+                localStorage.setItem('OfflineGTplaces', JSON.stringify(data));
+                loadPlaces();
+            }
+        });
    } else{
    		console.log("Your Browser Doesn't Support HTML5 Storage");
    }
@@ -106,7 +100,7 @@ function init() {
 
 
 function loadPlaces() {	
-	//Load sequential array into associative array
+	//Load places sequential array into associative array
 	var OfflineGTplaces = JSON.parse(localStorage.getItem('OfflineGTplaces'));
 	buildings = {};
 	$.each(OfflineGTplaces, function(){
@@ -114,34 +108,7 @@ function loadPlaces() {
     });
     
     //Build buildings list
-	populateList();
-	$.getJSON("api/tags/",function(data){
-	    console.log("Downloaded tags");
-    	localStorage.setItem('GTplacesTags', JSON.stringify(data));		
-	    loadTags();
-    });
-    
-    //Put name of building with supplied building id in search field
-    var bid = $.url().fparam("bid");
-    if (bid !== undefined && bid.length > 0) {
-        $.getJSON("api/buildings_id/"+bid, function(data){
-            $("input[data-type='search']").val(data[0].GTB_NAME).trigger('change');
-        });
-    }
-}
-
-
-//Add tags to each element in building listview for searching by tag
-function loadTags() {
-	tags = JSON.parse(localStorage.getItem('GTplacesTags'));	
-	for(var i = 0; i < tags.length; i++) {
-	    $("p#" + tags[i].GTB_BUILDING_NUMBER).append(tags[i].tag_name + ", ");
-	}	
-}
-
-
-function populateList() {
-    var listString;
+	var listString;
 	var templateRow = {};
 	$("#building_no_results_message").hide();
 	$("#searchResultsList").empty();
@@ -157,7 +124,18 @@ function populateList() {
 	} else {
 	    $("#building_no_results_message").show();
     }
-	library.changeLinksForOffline();	
+	library.changeLinksForOffline();
+	
+	//Get tags
+	$.getJSON("api/tags/",function(data){
+	    console.log("Downloaded tags");
+    	localStorage.setItem('GTplacesTags', JSON.stringify(data));		
+	    tags = JSON.parse(localStorage.getItem('GTplacesTags'));	
+	    //Add tags to each element in building listview for searching by tag
+	    for(var i = 0; i < tags.length; i++) {
+	        $("p#" + tags[i].GTB_BUILDING_NUMBER).append(tags[i].tag_name + ", ");
+	    }
+    });
 }
 
 
@@ -169,6 +147,7 @@ function populatePlaceInfo(placeInfo) {
 	$("#phone_num_link").empty();
 	
 	if (placeInfo.name != "") {
+	    //Handle place info
 		templateBuildingInfo.placeName = placeInfo.name;
 		templateBuildingInfo.placeImageUrl = placeInfo.image_url;
 		$("#buildingInfoTemplate").tmpl(templateBuildingInfo).appendTo( "#buildingDetailInfo" );
@@ -178,62 +157,54 @@ function populatePlaceInfo(placeInfo) {
 		buildingAddressInfo.phone_num = placeInfo.phone_num;		
 		$("#buildingAddressTemplate").tmpl(buildingAddressInfo).appendTo("#building_address_link");
 		$("#phoneNumberTemplate").tmpl(buildingAddressInfo).appendTo("#phone_num_link");
+		
+		//Handle tags
+		var placeTagList = {};
+	    $("#tags_list").empty();
+	    $("#noTaginfo").hide();
+	    if(placeInfo.tag_list[0] != "") {
+            var currTokens = placeInfo.tag_list;
+            for (var i = 0; i < currTokens.length; i++) {
+                placeTagList.buildingId = placeInfo.b_id;
+                placeTagList.buildingTag = currTokens[i];
+                $("#tagInfoTemplate").tmpl(placeTagList).appendTo("#tags_list");
+            }
+	    } else {
+		    $("#noTaginfo").show();
+	    }
 	} else {
 		alert("Place not found!");
 	}
 }
 
 
-function populatePlaceTags(placeTag) {
-	var placeTagList = {};
-	$("#tags_list").empty();
-	$("#noTaginfo").hide();
-	test = placeTag;
-
-	if(placeTag.tag_list != "") {
-        var currTokens = placeTag.tag_list;
-        for (var i = 0; i < currTokens.length; i++) {
-            placeTagList.buildingId = placeTag.b_id;
-            placeTagList.buildingTag = currTokens[i];
-            $("#tagInfoTemplate").tmpl(placeTagList).appendTo( "#tags_list" );
-        }
-	} else {
-		$("#noTaginfo").show();
-	}
-	
-}
-
-
 function saveTag() {
 	var newTag = $.trim($("#new_tag").val().toLowerCase());
-	
 	if (newTag.length != 0) {
-	    // Send a AJAX request to save the new tag
 		$.post("api/tags/", { "bid": currentPlaceID, "tag": newTag }, function(data) {
 			if("success" == $.trim(data)) {
 			    console.log("Saved tag");
 			    // Once the ajax request returns successfully, insert the new tag
 			    // into the buildingTags table and update the buildings tags string
 			    // so that the new tag can be used for searching
-			    $.map(tags, function (obj) {
-                    if(obj.GTB_BUILDING_NUMBER == currentPlaceID) {
-                        obj.tag_list.push(newTag);
-                        $("#tags_list #noTagsYet").remove();
-                        $("<span class='tag' id=" + currentPlaceID + "_" 
-                                + newTag + "'>" 
-                                + "<a href='#confirmFlagPopup' data-role='button'"
-                                + " data-inline='true' data-rel='dialog' data-transition='pop'>"
-                                + newTag + "</a>" +" </span> ")
-                        .appendTo("#tags_list").click(function() {
-                            confirmFlagTag(this);
-                         });
+                buildings[currentPlaceID].tag_list.push(newTag);
+                $("#tags_list #noTagsYet").remove();
+                $("<span class='tag' id=" + currentPlaceID + "_" 
+                        + newTag + "'>" 
+                        + "<a href='#confirmFlagPopup' data-role='button'"
+                        + " data-inline='true' data-rel='dialog' data-transition='pop'>"
+                        + newTag + "</a>" +" </span> ")
+                .appendTo("#tags_list").click(function() {
+                    confirmFlagTag(this);
+                 });
 
-                        $("#new_tag").val("");
-                        $("#tag_input").hide();
-                    }
-		       });
+                $("#new_tag").val("");
+                $("#tag_input").hide();
+                $("#noTaginfo").hide();
 			}
 		});
+	} else {
+	    alert("You didn't type a tag yet!");
 	}
 }
 
@@ -242,7 +213,6 @@ function searchResultClick(placeID) {
 	currentPlaceID = placeID;
 	console.log(buildings[placeID]);
 	populatePlaceInfo(buildings[placeID]);
-	getPlaceTags(placeID, populatePlaceTags);
 	library.changeLinksForOffline();
 }
 
