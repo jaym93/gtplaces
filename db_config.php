aa5bf6d920b946dd7677b87e146271a33e41f210
Andrew Nelson <andy@andyhub.com>
2015-05-15 14:20:41 -0400

    Updated credentials via Adam Arrowood
    The updated info is commented out so that we're still using our clone of
    the old database. Until someone gets real time to look into this, I'm
    leaving the new stuff commented out.

--- db_config.php
+++ db_config.php
@@ -4,11 +4,14 @@
     define('OLD_DATABASE', 'CORE_gtplaces');
     define('OLD_USERNAME', 'gtplaces');
     define('OLD_PASSWORD', 'QG3Zj3JrhNcSNsaC');
+    $db_host = "db.cip.gatech.edu";
+    $db_username = "gtplaces";
+    $db_password = "QG3Zj3JrhNcSNsaC";
+    $db_database = "CORE_gtplaces";
 
-    $db_host = "tlw-webapps.gatech.edu"; 
-    $db_username = "tlw-maps"; 
-    $db_password = "mappingGTcampus2468"; 
-    $db_database = "campusmap_GE";
-    
+    // New database
+    // $db_host = "tlw-auxdb1.gatech.edu";
+    // $db_username = "tlw_map_aux2";
+    // $db_password = "jwiejr*734Jf834usd83KRsx";
+    // $db_database = "tlw_map_aux";
 ?>
-
