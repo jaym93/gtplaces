25b5cbb07708a50f62d603ddd8ea28c441af93b5
Soorya Eswaran <soorya@ipsec-143-215-17-193.vpn.gatech.edu>
2017-03-30 00:40:43 -0400

    Removing db credentials from Github

--- db_config.php
+++ db_config.php
@@ -1,17 +1,11 @@
 <?php
     // now need the old database for LDAP names
-    define('OLD_HOST', 'db.cip.gatech.edu');
-    define('OLD_DATABASE', 'CORE_gtplaces');
-    define('OLD_USERNAME', 'gtplaces');
-    define('OLD_PASSWORD', 'QG3Zj3JrhNcSNsaC');
-    $db_host = "db.cip.gatech.edu";
-    $db_username = "gtplaces";
-    $db_password = "QG3Zj3JrhNcSNsaC";
-    $db_database = "CORE_gtplaces";
-
-    // New database
-    // $db_host = "tlw-auxdb1.gatech.edu";
-    // $db_username = "tlw_map_aux2";
-    // $db_password = "jwiejr*734Jf834usd83KRsx";
-    // $db_database = "tlw_map_aux";
+    define('OLD_HOST', getenv('OLD_HOST'));
+    define('OLD_DATABASE', getenv('OLD_DATABASE'));
+    define('OLD_USERNAME', getenv('OLD_USERNAME'));
+    define('OLD_PASSWORD', getenv('OLD_PASSWORD'));
+    $db_host = getenv('db_host');
+    $db_username = getenv('db_username');
+    $db_password = getenv('db_password');
+    $db_database = getenv('db_database');
 ?>
