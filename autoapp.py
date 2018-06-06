23a73092fef98a7558d8be16eff93036bcb3f03d
Jeremy Johnson <jeremy@imtc.gatech.edu>
2018-06-06 17:12:41 -0400

    Printing environment variables to debug OpenShift issue

--- autoapp.py
+++ autoapp.py
@@ -8,6 +8,10 @@ import os
 
 from api import create_app
 
+# DEBUG for OpenShift issue
+for key in os.environ.keys():
+    print("%30s %s \n" % (key,os.environ[key]))
+
 config_name = os.environ.get("ENV", "development")
 
 # Create the Flask app.
